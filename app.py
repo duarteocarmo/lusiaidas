7c199f8657bb34019f58068dceb84f0110a67223
Duarte OC <me@duarteocarmo.com>
2024-04-09 21:28:01 +0200

    checkpoint

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.background import BackgroundTasks
 from fastapi.responses import HTMLResponse, StreamingResponse
+from loguru import logger
 from llama_cpp import Llama
 from pydantic import BaseModel
+import time
+import typing as t
 import pathlib
 import json
 
 REPO_ID = "duarteocarmo/lusiaidas-v0.1-q4_k_m"
+CACHE_TIME_SECONDS = 7200
 GENERATION_CONFIG = {
     "max_tokens": 400,
     "temperature": 0.2,
@@ -14,7 +19,6 @@ GENERATION_CONFIG = {
     "repeat_penalty": 1.2,
     "stream": True,
 }
-LLM = Llama.from_pretrained(repo_id=REPO_ID, filename="*.gguf", verbose=True)
 PROMPT_PREFIX = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
 ### Instruction:
@@ -23,35 +27,55 @@ Escreve uma estrofe ao estilo de Os Lusíadas
 ### Response:
 {}"""
 
-test = "As coisas que eu gostava"
-
-# GENERATION_CONFIG.pop("stream")
-# print(LLM(**GENERATION_CONFIG, prompt=PROMPT_PREFIX.format(test)))
-
 
 class Prompt(BaseModel):
     prompt: str
 
 
+class LLMCache:
+    def __init__(self):
+        self.llm: t.Optional[Llama] = None
+
+    def load_model(self) -> Llama:
+        logger.info("Loading model")
+        if not self.llm:
+            self.llm = Llama.from_pretrained(
+                repo_id=REPO_ID, filename="*.gguf", verbose=True
+            )
+        return self.llm
+
+    def unload_model(self):
+        logger.info("Sleeping for 60 seconds")
+        time.sleep(CACHE_TIME_SECONDS)
+        if self.llm:
+            self.llm = None
+        logger.info("Model unloaded")
+
+
 app = FastAPI()
+llm_cache = LLMCache()
 
 
-def generate_completion(prompt: str):
-    print("Generating...")
+def generate_completion(prompt: str, model: Llama):
     completion = ""
-    for item in LLM(**GENERATION_CONFIG, prompt=PROMPT_PREFIX.format(prompt)):
+    for item in model(**GENERATION_CONFIG, prompt=PROMPT_PREFIX.format(prompt)):
         choice = item["choices"][0]  # type: ignore
-        completion += choice["text"]
+        completion += choice["text"]  # type: ignore
         yield "data: {}\n\n".format(json.dumps(choice))
 
-    print(prompt + completion)
+    logger.debug("Completion generated")
+    logger.debug(prompt + completion)
 
 
 @app.post("/completion")
-def completion(prompt: Prompt):
+def completion(prompt: Prompt, background_tasks: BackgroundTasks):
     if not prompt:
         raise HTTPException(status_code=400, detail="Prompt is required")
-    return StreamingResponse(generate_completion(prompt.prompt))
+
+    model = llm_cache.load_model()
+    background_tasks.add_task(llm_cache.unload_model)
+
+    return StreamingResponse(generate_completion(prompt.prompt, model=model))
 
 
 @app.get("/", response_class=HTMLResponse)
