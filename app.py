7fc16917a15ab85101161b8fba3538b2b6543f82
Duarte OC <me@duarteocarmo.com>
2024-04-10 19:26:04 +0200

    lusiaidas

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+# Description: This file contains the FastAPI application that serves the model.
+# It loads the model from the Hugging Face Hub and provides an endpoint to generate completions.
+# It also contains a timer to unload the model after a period of inactivity, because I have a small server.
+# Additionally, there is a lock to prevent multiple requests from generating completions at the same time.
+# This code is hacked together and not production-ready. It is only meant to be a proof of concept.
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse, StreamingResponse
 from loguru import logger
 from llama_cpp import Llama
-from pydantic import BaseModel
 import pathlib
 import json
-from threading import Lock
+from threading import Lock, Timer
+from pydantic import BaseModel
 
 REPO_ID = "duarteocarmo/lusiaidas-v0.1-q4_k_m"
-CACHE_TIME_SECONDS = 7200
+CACHE_TIME_SECONDS = 60 * 60
 GENERATION_CONFIG = {
     "max_tokens": 400,
     "temperature": 0.2,
@@ -24,32 +29,59 @@ Escreve uma estrofe ao estilo de Os Lusíadas
 
 ### Response:
 {}"""
-LLM = Llama.from_pretrained(repo_id=REPO_ID, filename="*.gguf", verbose=True)
+
+
 task_lock = Lock()
-
-
-class Prompt(BaseModel):
-    prompt: str
+model_timer = None
+LLM = None
 
 
 app = FastAPI()
 
 
-def generate_completion(prompt: str, model: Llama):
+def load_model():
+    global LLM
+    if LLM is None:
+        LLM = Llama.from_pretrained(repo_id=REPO_ID, filename="*.gguf", verbose=True)
+        logger.info("Model loaded")
+
+
+def unload_model():
+    global LLM
+    if LLM is not None:
+        LLM = None
+        logger.info("Model unloaded due to inactivity")
+
+
+def reset_timer():
+    global model_timer
+    if model_timer:
+        model_timer.cancel()
+    model_timer = Timer(CACHE_TIME_SECONDS, unload_model)
+    model_timer.start()
+    logger.info("Timer reset!")
+
+
+def generate_completion(prompt: str):
+    reset_timer()
+    load_model()
+
     task_lock.acquire()
     try:
-        logger.debug("Task lock acquired")
         completion = ""
-        for item in model(**GENERATION_CONFIG, prompt=PROMPT_PREFIX.format(prompt)):
+        assert LLM is not None, "Model not loaded"
+        for item in LLM(**GENERATION_CONFIG, prompt=PROMPT_PREFIX.format(prompt)):
             choice = item["choices"][0]  # type: ignore
             completion += choice["text"]  # type: ignore
             yield "data: {}\n\n".format(json.dumps(choice))
+
+        logger.debug("Completion generated")
     finally:
         task_lock.release()
-        logger.debug("Task lock released")
 
-    logger.debug("Completion generated")
-    logger.debug(prompt + completion)
+
+class Prompt(BaseModel):
+    prompt: str
 
 
 @app.post("/completion")
@@ -57,7 +89,7 @@ def completion(prompt: Prompt):
     if not prompt:
         raise HTTPException(status_code=400, detail="Prompt is required")
 
-    return StreamingResponse(generate_completion(prompt.prompt, model=LLM))
+    return StreamingResponse(generate_completion(prompt.prompt))
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -65,3 +97,16 @@ def index():
     return HTMLResponse(
         content=pathlib.Path("assets/index.html").read_text(), status_code=200
     )
+
+
+@app.on_event("startup")
+async def startup_event():
+    load_model()
+    reset_timer()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    global model_timer
+    if model_timer:
+        model_timer.cancel()
