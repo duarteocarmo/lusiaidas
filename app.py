23942a1404faf854a770e9eab12e45c1d30e9552
Duarte OC <me@duarteocarmo.com>
2024-04-10 18:50:30 +0200

    lusiaidas

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from llama_cpp import Llama
 from pydantic import BaseModel
 import pathlib
 import json
+from threading import Lock
 
 REPO_ID = "duarteocarmo/lusiaidas-v0.1-q4_k_m"
 CACHE_TIME_SECONDS = 7200
@@ -24,6 +25,7 @@ Escreve uma estrofe ao estilo de Os Lusíadas
 ### Response:
 {}"""
 LLM = Llama.from_pretrained(repo_id=REPO_ID, filename="*.gguf", verbose=True)
+task_lock = Lock()
 
 
 class Prompt(BaseModel):
@@ -34,11 +36,17 @@ app = FastAPI()
 
 
 def generate_completion(prompt: str, model: Llama):
-    completion = ""
-    for item in model(**GENERATION_CONFIG, prompt=PROMPT_PREFIX.format(prompt)):
-        choice = item["choices"][0]  # type: ignore
-        completion += choice["text"]  # type: ignore
-        yield "data: {}\n\n".format(json.dumps(choice))
+    task_lock.acquire()
+    try:
+        logger.debug("Task lock acquired")
+        completion = ""
+        for item in model(**GENERATION_CONFIG, prompt=PROMPT_PREFIX.format(prompt)):
+            choice = item["choices"][0]  # type: ignore
+            completion += choice["text"]  # type: ignore
+            yield "data: {}\n\n".format(json.dumps(choice))
+    finally:
+        task_lock.release()
+        logger.debug("Task lock released")
 
     logger.debug("Completion generated")
     logger.debug(prompt + completion)
